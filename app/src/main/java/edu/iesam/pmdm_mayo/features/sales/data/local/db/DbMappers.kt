acf0e8b95ad963cbfbb70e344ebdeed24dbf2bbf
Alumno <javier.lozher@educa.jcyl.es>
2025-05-20 18:50:53 +0200

    Fallo guardado Room apartado 3

--- app/src/main/java/edu/iesam/pmdm_mayo/features/sales/data/local/db/DbMappers.kt
+++ app/src/main/java/edu/iesam/pmdm_mayo/features/sales/data/local/db/DbMappers.kt
@@ -5,11 +5,14 @@ import edu.iesam.pmdm_mayo.features.sales.model.Sales
 
 fun Sales.toEntity(): SaleEntity {
     return SaleEntity(
+        id = 0,
         this.dni,
         this.conceptSales,
         this.totalSales
     )
 }
+
+
 
 fun SaleEntity.toDomain(): Sales {
     return Sales(
