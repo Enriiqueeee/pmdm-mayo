74528b3d161c76938d30c02d1a93cc430f26aa00
Alumno <javier.lozher@educa.jcyl.es>
2025-05-20 18:00:55 +0200

    Mejorado la vista del apartado 2

--- app/src/main/java/edu/iesam/pmdm_mayo/features/client/presentation/AddClientFragment.kt
+++ app/src/main/java/edu/iesam/pmdm_mayo/features/client/presentation/AddClientFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import edu.iesam.pmdm_mayo.R
 import edu.iesam.pmdm_mayo.databinding.AddClientFragmentBinding
 import edu.iesam.pmdm_mayo.features.client.domain.Client
 import org.koin.androidx.viewmodel.ext.android.viewModel
